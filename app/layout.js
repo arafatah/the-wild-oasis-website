aa509e8d1a69911b91e10b8353a18684c89cb0ff
arafatah <arafathowlader890@gmail.com>
2025-05-04 13:53:48 +0600

    chore: update styles and add primary/accent color palette in Tailwind config

--- app/layout.js
+++ app/layout.js
@@ -1,19 +1,21 @@
 import Logo from "@/app/_components/Logo";
 import Navigation from "@/app/_components/Navigation";
 
+import "@/app/_styles/globals.css";
+
 export const metadata = {
-  title: "The Blue Ridge"
-}
+  title: "The Blue Ridge",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className="bg-primary-950 text-primary-100 min-h-screen">
         <header>
           <Logo />
           <Navigation />
         </header>
-       
+
         <main>{children}</main>
         <footer>Copyright by The Blue Ridge</footer>
       </body>
