d60755a5c4e484af2a0126055e7c9513f42af622
arafatah <arafathowlader890@gmail.com>
2025-05-04 15:58:36 +0600

    Refactor Navigation and Header components for improved structure and styling

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,15 @@
 import Logo from "@/app/_components/Logo";
 import Navigation from "@/app/_components/Navigation";
 
+import { Josefin_Sans } from "next/font/google";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+
 import "@/app/_styles/globals.css";
+import Header from "./_components/Header";
 
 export const metadata = {
   title: {
@@ -15,14 +23,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-primary-950 text-primary-100 min-h-screen">
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
+      <body
+        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+      >
+        <Header />
 
         <main>{children}</main>
-        <footer>Copyright by The Blue Ridge</footer>
       </body>
     </html>
   );
