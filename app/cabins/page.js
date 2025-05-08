04226bca534a4ff9b1f14fc6dcc1f818e513f2ad
arafatah <arafathowlader890@gmail.com>
2025-05-08 12:43:36 +0600

    Add Filter component and integrate filtering functionality in CabinList; update page.js to pass filter prop

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,14 +1,17 @@
 import { Suspense } from "react";
 import CabinList from "../_components/CabinList";
 import Spinner from "../_components/Spinner";
+import Filter from "../_components/Filter";
 
 export const metadata = {
   title: "Cabin",
 };
 
-export const revalidate = 0; 
+// export const revalidate = 0;
 
-export default function Page() {
+export default function Page({ searchParams }) {
+  const filter = searchParams?.capacity ?? "all";
+
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -22,9 +25,12 @@ export default function Page() {
         home away from home. The perfect spot for a peaceful, calm vacation.
         Welcome to paradise.
       </p>
+      <div className="flex justify-end mb-8">
+        <Filter />
+      </div>
 
       <Suspense fallback={<Spinner />}>
-        <CabinList />
+        <CabinList filter={filter} />
       </Suspense>
     </div>
   );
