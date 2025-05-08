d984ba5a7ff34216413ecc02eace01ed4a0690cf
arafatah <arafathowlader890@gmail.com>
2025-05-08 10:42:37 +0600

    Refactor profile page to use standard img tag for country flag; remove unused Image import

--- app/account/profile/page.js
+++ app/account/profile/page.js
@@ -1,5 +1,4 @@
 import SelectCountry from "@/app/_components/SelectCountry";
-import Image from "next/image";
 
 export const metadata = {
   title: "Update profile",
@@ -41,9 +40,7 @@ export default function Page() {
         <div className="space-y-2">
           <div className="flex items-center justify-between">
             <label htmlFor="nationality">Where are you from?</label>
-            <Image
-              height={50}
-              width={50}
+            <img
               src={countryFlag}
               alt="Country flag"
               className="h-5 rounded-sm"
