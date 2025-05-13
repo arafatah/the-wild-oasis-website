ed10a3fba4bc77455d259cfd68fce71293430752
arafatah <arafathowlader890@gmail.com>
2025-05-13 11:59:58 +0600

    Implement Google sign-in functionality with SignInButton component and update authentication flow

--- app/account/page.js
+++ app/account/page.js
@@ -1,12 +1,16 @@
+import { auth } from "../_lib/auth";
 
 export const metadata = {
   title: "Guest setting",
 };
 
-export default function page() {
+export default async function page() {
+  const session = await auth();
+  const firstName = session?.user?.name.split(" ").at(0);
+
   return (
     <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-        Welcome, username
-      </h2>
-  )
+      Welcome, {session?.user?.name}{" "}
+    </h2>
+  );
 }
