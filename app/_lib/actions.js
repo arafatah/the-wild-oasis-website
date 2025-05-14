a1a8838170c09f4c3df042f3951aa7f26751524e
arafatah <arafathowlader890@gmail.com>
2025-05-14 17:05:13 +0600

    Implement deleteReservation function with authorization checks; enhance DeleteReservation component to confirm deletion and show loading state

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -3,6 +3,7 @@
 import { revalidatePath } from "next/cache";
 import { auth, signIn, signOut } from "./auth";
 import { supabase } from "./supabase";
+import { getBookings } from "./data-service";
 
 export async function updateGuest(formData) {
   const session = await auth();
@@ -10,11 +11,9 @@ export async function updateGuest(formData) {
 
   const nationalID = formData.get("nationalID");
   const [nationality, countryFlag] = formData.get("nationality").split("%");
-  
 
   if (!/^[a-zA-Z0-9]{6,12}$/.test(nationalID))
     throw new Error("Please provide a valid national ID");
-
 
   const updateData = { nationality, countryFlag, nationalID };
   const { data, error } = await supabase
@@ -24,7 +23,26 @@ export async function updateGuest(formData) {
 
   if (error) throw new Error("Guest could not be updated");
 
-  revalidatePath('/account/profile')
+  revalidatePath("/account/profile");
+}
+
+export async function deleteReservation(bookingId) {
+  const session = await auth();
+  if (!session) throw new Error("You must be logged in");
+
+  const guestBookings = await getBookings(session.user.guestId)
+  const guestBookingsIds = guestBookings.map((booking) => booking.id)
+
+  if(!guestBookingsIds.includes(bookingId)) throw new Error("You are not allowed to delete this booking")
+
+  const { data, error } = await supabase
+    .from("bookings")
+    .delete()
+    .eq("id", bookingId);
+
+  if (error) throw new Error("Booking could not be deleted");
+
+  revalidatePath('/account/reservations')
 }
 
 export async function singInAction() {
