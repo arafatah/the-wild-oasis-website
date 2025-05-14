7bc7175f3f822fdbadaa54c381384a03790154f5
arafatah <arafathowlader890@gmail.com>
2025-05-14 12:33:35 +0600

    Enhance UpdateProfileForm to accept guest data and update actions for guest profile management

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -1,20 +1,24 @@
 "use client";
 
 import { useState } from "react";
+import { updateGuest } from "../_lib/actions";
 
-function UpdateProfileForm({ children }) {
+function UpdateProfileForm({ guest, children }) {
   const [count, setCount] = useState();
 
-  // CHANGE
-  const countryFlag = "pt.jpg";
-  const nationality = "portugal";
+  const { fullName, email, nationality, nationalId, countryFlag } = guest;
 
   return (
-    <form className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col">
+    <form
+      action={updateGuest}
+      className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col"
+    >
       <div className="space-y-2">
         <label>Full name</label>
         <input
           disabled
+          name="fullName"
+          defaultValue={fullName}
           className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
         />
       </div>
@@ -23,6 +27,8 @@ function UpdateProfileForm({ children }) {
         <label>Email address</label>
         <input
           disabled
+          name="email"
+          defaultValue={email}
           className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
         />
       </div>
@@ -31,7 +37,9 @@ function UpdateProfileForm({ children }) {
         <div className="flex items-center justify-between">
           <label htmlFor="nationality">Where are you from?</label>
           <img
-            src={"https://upload.wikimedia.org/wikipedia/commons/5/5c/Flag_of_the_Taliban.svg"}
+            src={
+              "https://upload.wikimedia.org/wikipedia/commons/5/5c/Flag_of_the_Taliban.svg"
+            }
             alt="Country flag"
             className="h-5 rounded-sm"
           />
@@ -42,6 +50,7 @@ function UpdateProfileForm({ children }) {
       <div className="space-y-2">
         <label htmlFor="nationalID">National ID number</label>
         <input
+          defaultValue={nationalId}
           name="nationalID"
           className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
         />
--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -2,10 +2,14 @@
 
 import { signIn, signOut } from "./auth";
 
+export async function updateGuest( formData ) {
+  console.log(formData);
+}
+
 export async function singInAction() {
-    await signIn('google', {redirectTo: "/account"})
+  await signIn("google", { redirectTo: "/account" });
 }
 
 export async function signOutAction() {
-    await signOut({redirectTo: '/'})
+  await signOut({ redirectTo: "/" });
 }
